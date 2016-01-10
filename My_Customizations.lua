c314a045fcaca159516dad4c9dd8f1d1502cd658
Tyler Johnston <tylerjohnst@gmail.com>
2016-01-09 20:30:55 -0500

    Rename const

--- My_Customizations.lua
+++ My_Customizations.lua
@@ -4,7 +4,7 @@
 local SELFLESS_HEALER_ID = 114250
 local DENOUNCE_ID = 115654
 
-local SELFLESS = GetSpellInfo(SELFLESS_HEALER_ID)
+local SELFLESS_HEALER = GetSpellInfo(SELFLESS_HEALER_ID)
 local DENOUNCE = GetSpellInfo(DENOUNCE_ID)
 
 -- Create a frame to listen for events:
@@ -27,7 +27,7 @@ function AuraTickIfStacksExist(spellInfo, spellID, position, red, green, blue)
 end
 
 f:SetScript("OnEvent", function()
-	AuraTickIfStacksExist(SELFLESS, SELFLESS_HEALER_ID, "TOPRIGHT", 255, 255, 255)
+	AuraTickIfStacksExist(SELFLESS_HEALER, SELFLESS_HEALER_ID, "TOPRIGHT", 255, 255, 255)
 	AuraTickIfStacksExist(DENOUNCE, DENOUNCE_ID, "TOPLEFT", 255, 255, 255)
 end)
 
